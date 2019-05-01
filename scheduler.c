ebbdadc929509c0ed49715a194aa9f4756167dad
“Talgat <e178645@metu.edu.tr>
2019-05-02 01:00:45 +0300

    "minor changes"

--- scheduler.c
+++ scheduler.c
@@ -11,38 +11,45 @@
 #include <math.h>
 #include <time.h>
 
+struct ThreadInfo{
+    ucontext_t context;
+    int state;
+};
+
 
 int main(int argc, char *argv[])
 {
 
     // parsing command line arguments
-    int i=1;
-    int T[argc];
-    int tottime = 0;
-    for(i=1;i<argc;i++){
-        T[i] = atoi(argv[i]);
-        tottime += T[i]; 
-    };
+    if (argc <= 6){
+        int i=1;
+        int T[argc];
+        int tottime = 0;
+        for(i=1;i<argc;i++){
+            T[i] = atoi(argv[i]);
+            tottime += T[i]; 
+        };
 
-    printf("\nshare:\n");
+        printf("\nshare:\n");
 
-    int j = 1;
-    for(j=1;j<argc;j++){
-        
-        printf("\t %d/%d \t",T[j],tottime);
-    };
+        int j = 1;
+        for(j=1;j<argc;j++){
+            
+            printf("\t %d/%d \t",T[j],tottime);
+        };
 
-    printf("\n\nthreads:\n");
+        printf("\n\nthreads:\n");
 
-    int k = 1;
-    for(k=1;k<argc;k++){
-        
-        printf("\t  T%d \t",k);
-    };
+        int k = 1;
+        for(k=1;k<argc;k++){
+            
+            printf("\t  T%d \t",k);
+        };
 
-    printf("\n");
-
-
+        printf("\n");
+    }
+    else {printf("Error.\nDesign requirement: Number of threads cannot exceed 5\n");}
+    
 
 
 return 0;
